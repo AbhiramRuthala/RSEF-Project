30df50a9b7b19c0c4171b579bb704a628779a318
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2025-03-03 09:49:21 -0500

    Update AI-model.py

--- AI-model.py
+++ AI-model.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
+import librosa
+import numpy as np
+import os
 import sounddevice
+from coremltools.proto.NeuralNetwork_pb2 import AdamOptimizer
 from keras.src.metrics.accuracy_metrics import accuracy
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from nipype.testing import filepath
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Input
 import matplotlib.pyplot as plt
-
-
-#Not sure if we need these yet because we are only looking for the accuracy metric. It doesn't hurt to have other metrics though.
-from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
 
 print("Let's start by having the model evaluate your speech")
 
@@ -20,44 +22,50 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
+data_dir = 'trainingData'
+classes = ['Alzheimer-Speech', "Normal-Speech"]
 
-#Data collection/selection points.
+def preprocessData(data_dir, classes, target_shape=(128,128)):
+    data = []
+    labels = []
 
-#Separate data into batches
+    for i, class_name in enumerate(classes):
+        class_dir = os.path.join(data_dir, class_name)
+        for filename in os.listdir(class_dir):
+            if filename.endswith('.wav'):
+                filepathszn = os.path.join(class_dir, filename)
+                audio_data, sample_rate = librosa.load(filepathszn, sr=None)
 
-#Data analysis points from the batches.
+                mel_spectrogram = librosa.feature.mel_spectrogram(y=audio_data, sr=sample_rate)
+                mel_spectrogram = tf.image.resize(np.expand_dims(audio_data, axis=-1), target_shape)
+                data.append(mel_spectrogram)
+                labels.append(i)
 
+            return np.array(data), np.array(labels)
 
-#train_data
-#test_data
-#val_data
+data, labels = preprocessData(data_dir, classes)
+labels = tf.keras.utils.to_categorical(labels, num_classes=len(classes))
+X_train, X_test, Y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-#Need configuration of values to see which value represents Alzheimer's Disease or Normal voice (1 or 0)
+input_shape = X_train[0].shape
+input_layer = Input(shape = input_shape)
+x= Conv2D(32, (3, 3), activation='relu')(input_layer)
+x=MaxPooling2D((2,2))(x)
+x=Conv2D(64, (3,3 ), activation='relu')(x)
+x= MaxPooling2D((2,2))(x)
+x= Flatten()(x)
+x= Dense(64, activation='relu')(x)
+output_layer = Dense(len(classes), activation='softmax')(x)
+model = Model(input_layer, output_layer)
 
-#Skip certain parts of the data.
+model.compile(optimizer=AdamOptimizer(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-#train = dataszn.take(train_data)
-#test = dataszn.skip(train_data).take(test_data)
-#val = dataszn.skip(train_data+test_data).take(val_data)
+model.fit(X_train, Y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
 
+Accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(Accuracy[1])
 
-model = Sequential()
-
-model.add()
-
-model.add(Flatten())
-
-#Go from 256 neurons to 1 neuron.
-model.add(Dense(256, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile('adam', loss=tf.losses.BinaryCrossentropy, metrics=['accuracy'])
-
-#Run lines of code that provides the result of the data.
-
-#history = model.fit()
-
-#Plot the accuracy graph and display it at the end of the code.
+model.save('AlzSpeechMLmodel.h5')
 
 print(f"Here are the results of {name}'s examination with this tool.")
 
-print(f"Thanks for using it {name}")
